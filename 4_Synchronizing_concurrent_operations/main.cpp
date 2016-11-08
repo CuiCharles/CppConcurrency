cdb07dd7bc4b1d9b0c2f00530ea8264917b7c2a4
cuicharles <cuicharles@163.com>
2016-11-08 15:45:49 +0800

    add the chapter4test

---
+++ 4_Synchronizing_concurrent_operations/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <vector>
+#include <condition_variable>
+
+/*
+	std::condition_variable and std::condition_variable_any are both declared in <condition_variable> 
+	library header.They both need to work with an std::mutex in order to provide appropriate 
+	synchronization.
+
+	
+
+*/
+
+std::mutex mut;
+std::queue<int> data_queue;
+std::condition_variable data_cond;
+std::vector<int>  data_vec;
+int tip_index = 0;
+
+
+bool more_data_prepared()
+{
+	if(tip_index < 100)
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+
+int prepare_data()
+{
+	return data_vec[tip_index++];
+}
+
+bool is_last_chunk(int data)
+{
+	if(data == 100)
+		return true;
+	else 
+		return false;
+}
+
+void process_data(int);
+
+//productor
+void data_preparation_thread()
+{
+	while(more_data_prepared())
+	{
+		const int data = prepare_data();
+		std::lock_guard<std::mutex> lk(mut);
+		data_queue.push(data);
+		std::cout << "push data " << data << std::endl;
+		data_cond.notify_one();
+	}
+}
+
+
+//consumer
+void data_processing_thread()
+{
+	while(true)
+	{
+		std::unique_lock<std::mutex> lk(mut);
+		data_cond.wait(lk, []{ return !data_queue.empty();});
+		int data = data_queue.front();
+		data_queue.pop();
+		lk.unlock();
+		process_data(data);
+		if(is_last_chunk(data))
+		{
+			break;
+		}
+	}
+}
+
+void process_data(int data)
+{
+	std::cout << data << std::endl;
+}
+
+
+int main()
+{
+
+	for(int i = 0;i < 100; ++i)
+	{
+		data_vec.push_back(i + 1);
+	}
+
+	std::thread preparetion_thread(data_preparation_thread);
+	std::thread processing_thread(data_processing_thread);
+	preparetion_thread.join();
+	processing_thread.join();
+
+	return 0;
+}
