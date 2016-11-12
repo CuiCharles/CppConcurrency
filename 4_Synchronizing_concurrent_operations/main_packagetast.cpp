1a7230ec57d4acaa1e0ede7681609a5227a74341
cuicharles <cuicharles@163.com>
2016-11-12 10:41:23 +0800

    task with no void parameter and return value

--- 4_Synchronizing_concurrent_operations/main_packagetast.cpp
+++ 4_Synchronizing_concurrent_operations/main_packagetast.cpp
@@ -6,9 +6,11 @@
 #include <iostream>
 
 std::mutex m;
-std::deque<std::packaged_task<void()> > tasks;
+std::deque<std::packaged_task<int(int)> > tasks;
 
 bool is_shut_down = false;
+
+int x = 6;
 
 bool gui_shutdown_message_recieved()
 {
@@ -24,7 +26,7 @@ void gui_thread()
 	while(!gui_shutdown_message_recieved())
 	{
 		get_and_process_gui_message();
-		std::packaged_task<void()> task;
+		std::packaged_task<int(int)> task;
 		{
 			std::lock_guard<std::mutex> lk(m);
 			if(tasks.empty())
@@ -32,17 +34,17 @@ void gui_thread()
 			task = std::move(tasks.front());
 			tasks.pop_front();
 		}
-		task();
+		task(x++);
 	}
 }
 
 
 
 template<typename Func>
-std::future<void> post_task_for_gui_thread(Func f)
+std::future<int> post_task_for_gui_thread(Func f)
 {
-	std::packaged_task<void()> task(f);
-	std::future<void> res = task.get_future();
+	std::packaged_task<int(int)> task(f);
+	std::future<int> res = task.get_future();
 	std::lock_guard<std::mutex> lk(m);
 	tasks.push_back(std::move(task));
 	return res;
@@ -55,9 +57,14 @@ int main()
 
 	std::thread gui_bg_thread(gui_thread);
 
-	post_task_for_gui_thread([]{std::cout << "func1" << std::endl;});
-	post_task_for_gui_thread([]{std::cout << "func2" << std::endl;});
-	post_task_for_gui_thread([]{std::cout << "func3" << std::endl;});
+	std::future<int> f1 = post_task_for_gui_thread([](int a) -> int{std::cout << "func1" << std::endl; return a;});
+	std::future<int> f2 = post_task_for_gui_thread([](int a) -> int{std::cout << "func2" << std::endl; return a;});
+	std::future<int> f3 = post_task_for_gui_thread([](int a) -> int{std::cout << "func3" << std::endl; return a;});
+
+	std::cout << "future1" << f1.get() << std::endl;
+	std::cout << "future2" << f2.get() << std::endl;
+	std::cout << "future3" << f3.get() << std::endl;
+
 	getchar();
 	is_shut_down = true;
 	gui_bg_thread.join();
